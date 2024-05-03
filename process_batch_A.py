59808538a8e3dd741b47a5384ea46047e44af19d
S573647 <S573647@nwmissouri.edu>
2024-05-03 18:22:08 -0500

    Commit the changes to create temperature file from F to K and K to F

--- process_batch_A.py
+++ process_batch_A.py
@@ -28,6 +28,7 @@ logging.basicConfig(
 
 INPUT_FILE_NAME = "batchfile_0_farenheit.csv"
 OUTPUT_FILE_NAME = "batchfile_1_celcius.csv"
+OUTPUT_FILE_K_NAME = "batchfile_2_kelvin.csv"
 
 # Define program functions (bits of reusable code)
 
@@ -42,8 +43,59 @@ def convert_f_to_c(temp_f):
     logging.debug(f"Converted {temp_f}F to {celsius}C.")
     return celsius
 
+# Define program functions (bits of reusable code)
 
-def process_rows(input_file_name, output_file_name):
+def convert_f_to_k(temp_f):
+    """Convert Fahrenheit to Kelvin.
+    Use the built-in round() function to round to 2 decimal places
+    Use the built-in float() function to convert the string to a float (a floating point number)
+    All CSV values are read as strings.
+    """
+    logging.debug(f"Calling convert_f_to_k() with {temp_f}.")
+    kelvin = round((float(temp_f) - 32.0) * 5.0 / 9.0 + 273.15, 2)
+    logging.debug(f"Converted {temp_f}F to {kelvin}K.")
+    return kelvin
+
+
+def process_rows_in_k(input_file_name, output_file_name):
+    """Read from input file, convert temperature, and write to output file."""
+    logging.info(f"Calling process_rows(): {input_file_name} to {output_file_name}.")
+
+    # Create a file object for input (r = read access)
+    with open(input_file_name, "r") as input_file:
+        logging.info(f"Opened for reading: {input_file_name}.")
+
+        # Create a CSV reader object
+        reader = csv.reader(input_file, delimiter=",")
+
+        header = next(reader)  # Our file has a header row, move to next to get to data
+        logging.info(f"Skipped header row: {header}")
+
+        # Create a file object for output (w = write access)
+        # Set the newline parameter to an empty string to avoid extra newlines in the output file
+        with open(output_file_name, "w", newline="") as output_file:
+            logging.info(f"Opened for writing: {output_file_name}.")
+
+            # Create a CSV writer object
+            writer = csv.writer(output_file, delimiter=",")
+
+            # Write the header row to the output file
+            writer.writerow(["Year", "Month", "Day", "Time", "TempK"])
+
+            # For each data row in the reader
+            for row in reader:
+                # Extract the values from the input row into named variables
+                Year, Month, Day, Time, TempF = row
+
+                # Call the conversion function, passing in the TempF argument
+                # Assign the return value to a new variable named TempC
+                TempK = convert_f_to_k(TempF)
+
+                # Write the transformed data to the output file
+                writer.writerow([Year, Month, Day, Time, TempK])
+
+
+def process_rows_in_c(input_file_name, output_file_name):
     """Read from input file, convert temperature, and write to output file."""
     logging.info(f"Calling process_rows(): {input_file_name} to {output_file_name}.")
 
@@ -89,7 +141,10 @@ if __name__ == "__main__":
     try:
         logging.info("===============================================")
         logging.info("Starting batch process A.")
-        process_rows(INPUT_FILE_NAME, OUTPUT_FILE_NAME)
+        logging.info("Converting F --> C")
+        process_rows_in_c(INPUT_FILE_NAME, OUTPUT_FILE_NAME)
+        logging.info("Converting F --> K")
+        process_rows_in_k(INPUT_FILE_NAME, OUTPUT_FILE_K_NAME)
         logging.info("Processing complete! Check for new file.")
         logging.info("===============================================")
     except Exception as e:
